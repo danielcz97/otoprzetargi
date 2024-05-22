0aec45afc32890bb201c1b8100aa7dd632d13038
Daniel Czerepak <firma.dcode@gmail.com>
2024-05-22 11:20:09 +0200

    tt

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://hammerhead-app-o9ma2.ondigitalocean.app/'),
 
     /*
     |--------------------------------------------------------------------------
