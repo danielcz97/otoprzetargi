31b44fcab2212fb88ee071a621891c4903d03c79
Daniel Czerepak <firma.dcode@gmail.com>
2024-05-22 15:17:37 +0200

    added env and possibility to login to panel

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,8 +9,8 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $promotedNodes = Property::where('promote', 1)->limit(10)->get();
-        $latestNodes = Property::orderBy('created', 'desc')->take(20)->get();
+        $promotedNodes = Property::where('promote', 1)->orderBy('created', 'desc')->limit(10)->get();
+        $latestNodes = Property::orderBy('created', 'desc')->ake(20)->get();
         $latestPosts = Post::latest()->take(6)->get();
 
         return view('welcome', compact('promotedNodes', 'latestNodes', 'latestPosts'));
