2d9514b3cd9551958852525849846f8cdbdc5c0e
Daniel Czerepak <firma.dcode@gmail.com>
2024-05-22 12:16:36 +0200

    yesyh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+if($this->app->environment('production')) {
+    \URL::forceScheme('https');
+}
+
     }
 
     /**
