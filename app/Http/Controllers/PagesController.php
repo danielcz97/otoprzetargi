f977e25281aa986ec5375a7a356a5774450b2557
Daniel Czerepak <firma.dcode@gmail.com>
2024-05-22 15:39:35 +0200

    mistake

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,7 @@ class PagesController extends Controller
     public function index()
     {
         $promotedNodes = Property::where('promote', 1)->orderBy('created', 'desc')->limit(10)->get();
-        $latestNodes = Property::orderBy('created', 'desc')->ake(20)->get();
+        $latestNodes = Property::orderBy('created', 'desc')->limit(20)->get();
         $latestPosts = Post::latest()->take(6)->get();
 
         return view('welcome', compact('promotedNodes', 'latestNodes', 'latestPosts'));
