56a298db330da87c557409bfe6103f89de207b90
Daniel Czerepak <firma.dcode@gmail.com>
2024-06-08 14:12:37 +0200

    fix

--- resources/views/node/index.blade.php
+++ resources/views/node/index.blade.php
@@ -83,7 +83,7 @@
                             <i style="font-size:25px; color:red" class="fas fa-print">Drukuj</i>
                         </a>
                     </div>
-                    <img style="max-width:250px" src="{{ $firstMediaUrl }}">
+                    <img style="max-width:250px" src="{{ $mainMediaUrl }}">
                 </div>
             </div>
         </div>
